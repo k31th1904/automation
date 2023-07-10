16b3ae2f2744d88156ef62c0f29c0bb5055edfb1
k31th1904 <keithkwok1904@gmail.com>
2023-07-10 17:40:56 -0400

    add lab07s1

---
+++ automation/terraform/lab07s1/modules/linux/variables.tf
@@ -0,0 +1,48 @@
+variable "rg" {}
+variable "location" {}
+variable "subnet_id" {}
+variable "vmname" {}
+variable "vmsize" {}
+variable "adminusername" {
+  default = "n01595368"
+}
+variable "publickey" {
+  default = "~/.ssh/id_rsa.pub"
+}
+variable "privatekey" {
+  default = "~/.ssh/id_rsa"
+}
+variable "disk_attributes" {
+  type = map(any)
+  default = {
+    type    = "Standard_LRS"
+    size    = 32
+    caching = "ReadWrite"
+  }
+}
+variable "OS_attribute" {
+  type = map(any)
+  default = {
+    publisher = "OpenLogic"
+    offer     = "CentOS"
+    sku       = "8_2"
+    version   = "latest"
+  }
+}
+variable "linux_avs_att" {
+  type = map(any)
+  default = {
+    platform_update_domain_count = "5"
+    platform_fault_domain_count  = "2"
+  }
+}
+variable "linux_avs_name" {}
+variable "nb_count" {}
+locals {
+  common_tags = {
+    Name         = "Terraform-Class"
+    Project      = "Linux"
+    ContactEmail = "n01595368@humber.ca"
+    Environment  = "Lab"
+  }
+}
---
+++ automation/terraform/lab07s1/modules/windows/variables.tf
@@ -0,0 +1,43 @@
+variable "rg" {}
+variable "location" {}
+variable "subnet_id" {}
+variable "winadminusername" {
+  default = "winadm"
+}
+variable "winpassword" {
+  default = "Winadm!23"
+}
+variable "windisk_attributes" {
+  type = map(any)
+  default = {
+    type    = "StandardSSD_LRS"
+    size    = 128
+    caching = "ReadWrite"
+  }
+}
+variable "winOS_attribute" {
+  type = map(any)
+  default = {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+}
+variable "windows_avs_att" {
+  type = map(any)
+  default = {
+    platform_update_domain_count = "5"
+    platform_fault_domain_count  = "2"
+  }
+}
+variable "windows_avs_name" {}
+variable "windows_name" {}
+locals {
+  common_tags = {
+    Name         = "Terraform-Class"
+    Project      = "Windows"
+    ContactEmail = "n01595368@humber.ca"
+    Environment  = "Lab"
+  }
+}
